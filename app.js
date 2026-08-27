ce7881b2dbdfaf4b683cc3064cf19a1271a886fb
chathulks <sadeeshachathumal5@gmail.com>
2026-08-27 23:19:21 +0530

    feat: implement loadApplicationTable function to fetch and display user data

--- app.js
+++ app.js
@@ -282,3 +282,51 @@ function new_customerAdd() {
         });
 
 }
+
+function loadApplicationTable() {
+    fetch("https://api.freeprojectapi.com/api/BankLoan/GetAllUsers")
+        .then(response => response.json())
+        .then(data => {
+            const user = data.data;
+            let body = `
+                    <caption>List of users</caption>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Full Name</th>
+                            <th>Role</th>
+                            <th>Created Date</th>
+                            <th>Password</th>
+                            <th class="d-flex justify-content-center align-items-center">Action</th>
+                        </tr>
+                    </thead>
+            `;
+
+            user.forEach(element => {
+                body += `
+                    <tbody>
+                        <tr>
+                            <td class="bg-danger bg-opacity-75 text-white"><div class="d-flex align-items-center m-1">${element.userId}</div></td>
+                            <td><div class="d-flex align-items-center m-1">${element.userName}</div></td>
+                            <td><div class="d-flex align-items-center m-1">${element.emailId}</div></td>
+                            <td><div class="d-flex align-items-center m-1">${element.fullName}</div></td>
+                            <td class="bg-dark bg-opacity-75 text-white"><div class="d-flex align-items-center m-1">${element.role}</div></td>
+                            <td><div class="d-flex align-items-center m-1">${element.createdDate}</div></td>
+                            <td><div class="d-flex m-1 align-items-center">${element.password}</div></td>
+                            <td class="d-flex justify-content-center align-items-center">
+                                <button class="btn border-0 p-0 m-1 delete-btn">
+                                    <i class="bi bi-trash3-fill text-danger" style="font-size: 17px;" onclick="deleteUser()"></i>
+                                </button>
+                                <button class="btn border-0 p-0 m-1 update-btn">
+                                    <i class="bi bi-pencil-square text-primary" style="font-size: 17px;" onclick="updateUserDataSet()"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                `;
+            });
+            document.getElementById("user-table").innerHTML = body;
+        });
+}
